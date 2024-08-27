3b3888e2868e80677c01bb040de58a2af78f1ede
Ackerman, Adam P. <adam@ou.edu>
2024-08-27 10:12:39 -0500

    COmpleted in class activity over wpf

---
+++ In_Class_Examples/FirstWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtFullname.Text;
+            string address = txtAddress.Text;
+
+
+            if (string.IsNullOrWhiteSpace(name) == true ||
+                string.IsNullOrWhiteSpace(address) == true)
+            {
+                MessageBox.Show($"Invalid Input!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Student s = new Student();
+            s.FirstName = name.Split(' ')[0];
+            s.LastName = name.Split(' ')[1];
+            s.Address = address;
+
+            lstStudents.Items.Add(s);
+            ClearForm();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            // Clear the textboxes
+            txtFullname.Text = "";
+            txtAddress.Clear();
+            txtFullname.Focus();
+        }
+
+        private void lstStudents_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Student selectedStudent = (Student)lstStudents.SelectedItem;
+
+            MessageBox.Show($"Name: {selectedStudent.FirstName} {selectedStudent.LastName}\nAddress: {selectedStudent.Address}", "Student Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ In_Class_Examples/FirstWPFApplication/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWPFApplication
+{
+    public class Student
+    {
+        public  string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public Student()
+        {
+            FirstName = "";
+            LastName = "";
+            Address = "";
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}

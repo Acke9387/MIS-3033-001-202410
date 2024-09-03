c681517c3e15f7040f2f163f36f9b039892bf759
Ackerman, Adam P. <adam@ou.edu>
2024-09-03 09:22:06 -0500

    Completed first participation over wpf

---
+++ Participations/FirstWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> students = new List<Student>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            wndMain.Background = Brushes.Red;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            //DateTime? birthdate = dpBirthdate.SelectedDate;
+            DateTime bday = Convert.ToDateTime(txtBirthday.Text);
+
+            //DateTime.Parse(txtBirthday.Text);
+
+            Student student = new Student();
+            student.Name = name;
+            student.Birthdate = bday;
+
+            //students.Add(student);
+            lstStudents.Items.Add(student);
+        }
+
+        private void btnAdd_MouseEnter(object sender, MouseEventArgs e)
+        {
+            wndMain.Background = Brushes.Blue;
+        }
+
+        private void btnAdd_MouseLeave(object sender, MouseEventArgs e)
+        {
+            wndMain.Background = Brushes.Red;
+        }
+    }
+}
---
+++ Participations/FirstWPFApplication/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing.IndexedProperties;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWPFApplication
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
+
+        public Student()
+        {
+            Name = string.Empty;
+            Birthdate = null;
+        }
+
+        public int CalculateAge()
+        {
+            int age = 0;
+
+            TimeSpan timeSpan = DateTime.Now - Birthdate.Value;
+
+            //return timeSpan.Days / 365;
+
+            // Calculate the age of the student
+            if (Birthdate.HasValue)
+            {
+                DateTime today = DateTime.Today;
+                age = today.Year - Birthdate.Value.Year;
+                if (Birthdate.Value > today.AddYears(-age))
+                {
+                    age--;
+                }
+            }
+
+            return age;
+        }
+
+        public override string ToString()
+        {
+            return Name + " - " + Birthdate.Value.ToShortDateString();
+        }
+
+    }
+}

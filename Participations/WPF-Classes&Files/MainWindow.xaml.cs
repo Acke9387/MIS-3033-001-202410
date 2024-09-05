66b91fc565413f0ef6973fc0741dc8fb3d03637c
Ackerman, Adam P. <adam@ou.edu>
2024-09-05 09:29:47 -0500

    completed participation over wpf and classes

--- Participations/WPF-Classes&Files/MainWindow.xaml.cs
+++ Participations/WPF-Classes&Files/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,8 +20,53 @@ namespace WPF_Classes_Files
         public MainWindow()
         {
             InitializeComponent();
+
+            //string[] linesOfFile = File.ReadAllLines("Toys.csv");
+            var linesOfFile = File.ReadAllLines("Toys.csv").Skip(1);
+
+            foreach (var line in linesOfFile)
+            {
+                var pieces = line.Split(",");
+                //   0          1     2     3
+                //Manufacturer,Name,Price,Image
+                Toy t = new Toy();
+                t.Manufacturer = pieces[0];
+                t.Name = pieces[1];
+                t.Price = Convert.ToDouble(pieces[2]);
+                t.Image = pieces[3];
+
+                lstToys.Items.Add(t);
+            }
         }
 
-        
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Toy t = new Toy();
+            t.Name = txtName.Text;
+            t.Manufacturer = txtManufacturer.Text;
+            t.Price = Convert.ToDouble(txtPrice.Text);
+            t.Image = txtImage.Text;
+            
+            lstToys.Items.Add(t);
+            
+            txtName.Clear();
+            txtManufacturer.Clear();
+            txtPrice.Clear();
+            txtImage.Clear();
+        }
+
+        private void lstToys_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //Toy selected = (Toy)lstToys.SelectedItem ;
+            Toy selected = lstToys.SelectedItem as Toy;
+            if (selected != null)
+            {
+                Uri uri = new Uri(selected.Image);
+                BitmapImage bitmap = new BitmapImage(uri);
+                imgToy.Source = bitmap;
+                MessageBox.Show(selected.GetAisle());
+                //imgToy.Source = new BitmapImage(new Uri(selected.Image, UriKind.Relative));
+            }
+        }
     }
 }
--- Participations/WPF-Classes&Files/Toy.cs
+++ Participations/WPF-Classes&Files/Toy.cs
@@ -8,6 +8,33 @@ namespace WPF_Classes_Files
 {
     public class Toy
     {
+        public string Manufacturer { get; set; }
+        public string  Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Image = "";
+            Aisle = "";
+        }
+
+        public string GetAisle()
+        {
+            return $"{Manufacturer.ToUpper()[0]}{Price.ToString().Replace("$", "").Replace(".", "").Replace(",","")}";
+        }
+
+        public override string ToString()
+        {
+            return $"{Manufacturer} {Name} {Price.ToString("C")}";
+        }
 
     }
 }

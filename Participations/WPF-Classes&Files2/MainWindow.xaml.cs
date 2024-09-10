c3e02abd637882b47235e6d69839d5bb4e62357f
Ackerman, Adam P. <adam@ou.edu>
2024-09-10 09:27:50 -0500

    Completed participation

---
+++ Participations/WPF-Classes&Files2/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes_Files2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var linesOfFile = File.ReadAllLines("SalesJan2009.csv");
+
+            foreach (var line in linesOfFile.Skip(1))
+            {
+                Sale currentSale;// = new Sale();
+                var pieces = line.Split(',');
+
+                //        0            1     2       3         4    5     6       7       8               9           10          11
+                //Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
+                //currentSale.Price = Double.Parse(pieces[2]);
+                //currentSale.PaymentType= pieces[3];
+                currentSale = new Sale(Double.Parse(pieces[2]), pieces[3]);
+                currentSale.Name = pieces[4];
+                currentSale.Country = pieces[7];
+                currentSale.TransactionDate = DateTime.Parse(pieces[0]);
+
+                if (currentSale.PaymentType.ToLower() == "visa")
+                {
+                    lstVisa.Items.Add(currentSale);
+                }
+                else if (currentSale.PaymentType.ToLower() == "amex")
+                {
+                    lstAmex.Items.Add(currentSale);
+                }
+                else if (currentSale.PaymentType.ToLower() == "mastercard")
+                {
+                    lstMastercard.Items.Add(currentSale);
+                }
+                else if (currentSale.PaymentType.ToLower() == "diners")
+                {
+                    lstDiners.Items.Add(currentSale);
+                }
+                else
+                {
+                    MessageBox.Show($"{currentSale.PaymentType} is not a valid payment type for.");
+                }
+            }
+
+            lblMasterCount.Content = lstMastercard.Items.Count;
+        }
+
+        private void lstMastercard_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = lstMastercard.SelectedItem as Sale;
+            if (selected != null)
+            {
+                MessageBox.Show($"{selected.Country}");
+            }
+        }
+
+        private void lstVisa_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = lstVisa.SelectedItem as Sale;
+            if (selected != null)
+            {
+                MessageBox.Show($"{selected.Country}");
+            }
+        }
+
+        private void lstDiners_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = lstDiners.SelectedItem as Sale;
+            if (selected != null)
+            {
+                MessageBox.Show($"{selected.Country}");
+            }
+        }
+
+        private void lstAmex_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = lstAmex.SelectedItem as Sale;
+            if (selected != null)
+            {
+                MessageBox.Show($"{selected.Country}");
+            }
+        }
+    }
+}
---
+++ Participations/WPF-Classes&Files2/Sale.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classes_Files2
+{
+    public class Sale
+    {
+
+        public DateTime TransactionDate { get; set; }
+        public double Price { get; set; }
+        public string PaymentType { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public Sale()
+        {
+            TransactionDate = DateTime.Now;
+            Price = 0.0;
+            PaymentType = string.Empty;
+            Name = string.Empty;
+            Country = string.Empty;
+        }
+
+        public Sale(double price, string pType) : this()
+        {
+            //TransactionDate = DateTime.Now;
+            Price = price;
+            PaymentType = pType;
+            //Name = string.Empty;
+            //Country = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"[{PaymentType[0]}] {Name} - {Price.ToString("C")}";
+        }
+    }
+}

818c834aec6b0b3f6561a78206e5dd4683431024
Ackerman, Adam P. <adam@ou.edu>
2024-09-10 10:02:45 -0500

    did in class participation over json

---
+++ In_Class_Examples/JSON_Example/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var json = File.ReadAllText("sales_data.json");
+            List<Sale> salesData = JsonConvert.DeserializeObject<List<Sale>>(json);
+
+            lstData.ItemsSource = salesData;
+
+            //foreach (var sale in salesData)
+            //{
+            //    lstData.Items.Add(sale);
+            //}
+
+        }
+    }
+}
---
+++ In_Class_Examples/JSON_Example/Sale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Example
+{
+    public class Sale
+    {
+        public int customer_id { get;set; }
+
+        public DateTime purchase_date { get; set; }
+
+        public double total_amount { get; set; }
+
+        public Sale()
+        {
+            customer_id = 0;
+            purchase_date = DateTime.Now;
+            total_amount = 0.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Customer ID: {customer_id}, Purchase Date: {purchase_date}, Total Amount: {total_amount}";
+        }
+
+    }
+}

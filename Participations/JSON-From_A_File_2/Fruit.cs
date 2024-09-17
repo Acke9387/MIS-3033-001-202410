1b507a62387146c302651eea39db57d4cad766fa
adamackrmn <adamackrmn@gmail.com>
2024-09-17 17:28:37 -0500

    committing code from json from a file 2

---
+++ Participations/JSON-From_A_File_2/Fruit.cs
@@ -0,0 +1,23 @@
+﻿namespace JSON_From_A_File_2
+{
+    public class Fruit
+    {
+
+        public string name { get; set; }
+        public string image { get; set; }
+
+        public double price { get; set; }
+
+        public Fruit()
+        {
+            name = "";
+            image = "";
+            price = 0.0;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ Participations/JSON-From_A_File_2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_From_A_File_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string json = File.ReadAllText("fruits.json");
+
+            Market m = JsonConvert.DeserializeObject<Market>(json);
+
+            foreach (Fruit f in m.fruits)
+            {
+                cboFruits.Items.Add(f);
+            }
+
+
+        }
+
+        private void cboFruits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Fruit selected = (Fruit)cboFruits.SelectedItem;
+            txtName.Text = selected.name;
+            txtPrice.Text = selected.price.ToString("C");
+            imgFruit.Source = new BitmapImage(new Uri(selected.image));
+        }
+    }
+}
